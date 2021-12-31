a408dc31936690e90ae6c966fd99b7662ad06ef4
newhorizon-tech <alaukikinbox@gmail.com>
2022-01-01 01:13:12 +0530

    Fix ESLint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 
 const App = () => {
-  const list = ["One", 'Two', 'Three']
+  const list = ['One', 'Two', 'Three'];
   return (
     <div className="App">
       <h1>Fresh Feed</h1>
       <ul id="main-feed">
-         {list.map(item => <li className="feed-items"> {item} </li>)}
-       </ul>
+        {list.map((item) => (
+          <li key={list.indexOf(item)} className="feed-items">
+            {' '}
+            {item}
+            {' '}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
