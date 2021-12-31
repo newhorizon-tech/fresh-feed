763090bf7d56fa747f40caf0c1bf898e09d239ce
newhorizon-tech <alaukikinbox@gmail.com>
2022-01-01 01:04:51 +0530

    Add puppeteer

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const path = require('path')
+
 const app = express();
+const puppeteer = require('puppeteer');
 
 
-app.get('/user/:user', (req, res) => {
+
+app.get('/user/:user', async (req, res) => {
   const username = req.params.user;
-  console.log(username)
-  res.send(`User: ${username}`)
-})
+  await scrape(username, twitterUrl);
+  res.send(`User: ${username}`);
+});
 
-app.get('/test/', (req, res) => res.send('Node.js/Express works!'))
+app.get('/test/', (req, res) => res.send('Node.js/Express works!'));
 
-app.use(express.static('build'))
+app.use(express.static('build'));
 
-
-app.listen(8080)
+app.listen(8080);
