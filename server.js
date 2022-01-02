aad3e1ea4e83553c04712ef435ca718a9618519a
newhorizon-tech <alaukikinbox@gmail.com>
2022-01-02 17:07:51 +0530

    Minor fixes

--- server.js
+++ server.js
@@ -6,37 +6,40 @@ const puppeteer = require('puppeteer');
 const twitterUrl = 'https://twitter.com/';
 
 const mergeArrayIntoSet = (arr, set) => {
-  arr.forEach((e) => set.add(e))
-}
+  arr.forEach((e) => set.add(e));
+};
 
 const getTweets = async (page) => {
   const data = await page.evaluate(() => {
-    const timeline = document.querySelector("section div");
-    const content = timeline.querySelectorAll("article div[lang][dir]");
+    const timeline = document.querySelector('section div');
+    const content = timeline.querySelectorAll('article div[lang][dir]');
     const lastElement = content[content.length - 1];
-    const tweetsText = Array.from(content, e => e.textContent.trim());
+    const tweetsText = Array.from(content, (e) => e.textContent.trim());
     lastElement.scrollIntoView();
     return tweetsText;
-  }, );
-  return data
-}
+  });
+  return data;
+};
 
 const scrape = async (username, twitterUrl, puppeteer) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   page.goto(`${twitterUrl}${username}?lang=en`);
   await page.waitForXPath(
-    "//div[contains(@aria-label,'Timeline')]"
+    "//div[contains(@aria-label,'Timeline')]",
   );
-  await console.log("loaded")
-  await page.waitForTimeout(100)
+  await console.log('loaded');
+  await page.waitForTimeout(100);
 
   const tweetSet = new Set([]);
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i += 1) {
     const tweetsText = await getTweets(page);
-    mergeArrayIntoSet(tweetsText, tweetSet)
-    await page.waitForTimeout(100)
+    await page.waitForTimeout(200);
+    const tweetsBefore = tweetSet.size;
+    mergeArrayIntoSet(tweetsText, tweetSet);
+    const tweetsAfter = tweetSet.size;
+    console.log({ tweetsBefore, tweetsAfter });
   }
 
   return tweetSet;
@@ -44,20 +47,20 @@ const scrape = async (username, twitterUrl, puppeteer) => {
 
 app.get('/user/:user', async (req, res) => {
   const username = req.params.user;
-  let tweets = await scrape(username, twitterUrl, puppeteer);
-  let tweetsObj = {}
+  const tweets = await scrape(username, twitterUrl, puppeteer);
+  const tweetsObj = {};
   tweets.forEach((x, i) => {
     console.log(i);
     console.log(JSON.stringify(x));
     tweetsObj[i] = {
       id: i,
-      content: x
-    }
+      content: x,
+    };
     console.log('---------------------------');
-  })
+  });
   res.set({
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   });
 
   res.json(tweetsObj);
