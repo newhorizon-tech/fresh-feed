8bd73f0b10ebe75f7bd51c2445dddf2e2eed51ef
newhorizon-tech <alaukikinbox@gmail.com>
2022-01-02 17:05:23 +0530

    Add PropTypes

--- src/components/feed-display.js
+++ src/components/feed-display.js
@@ -1,15 +1,22 @@
+import PropTypes from 'prop-types';
+
 const FeedDisplay = (props) => {
-  const {list} = props;
-  return(
+  const { list } = props;
+  return (
     <ul id="main-feed">
       {
-       Object.values(list).map(item => (
-        <li key={item.id} className="feed-items">
-          {item.content}
-        </li>
-      ))}
-    </ul>
-  )
+       Object.values(list).map((item) => (
+         <li key={item.id} className="feed-items">
+           {item.content}
+         </li>
+       ))
 }
+    </ul>
+  );
+};
 
 export default FeedDisplay;
+
+FeedDisplay.propTypes = {
+  list: PropTypes.object.isRequired,
+};
